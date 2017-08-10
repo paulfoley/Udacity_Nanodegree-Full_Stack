2da76c9a83de58ec4861173a2758e01e32319c0c
Paul Foley <me@paulnicholasfoley.com>
2017-08-10 14:29:25 -0600

    Add Vagrant Projects to Repositories

---
+++ vagrant/Employees/employees.py
@@ -0,0 +1,27 @@
+
+## Imports 
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Employee(Base):
+	__tablename__ = 'employee'
+	
+	name = Column(String(250), nullable = False)
+	id = Column(Integer, primary_key = True)
+
+class Address(Base):
+	__tablename__ = 'address'
+
+	street = Column(String(80), nullable = False)
+	zip = Column(String(5), nullable = False)
+	id = Column(Integer, primary_key = True)
+	employee_id = Column(Integer, ForeignKey('employee.id'))
+	employee = relationship(Employee)
+
+engine = create_engine('sqlite:///employeeData.db')
+Base.metadata.create_all(engine)
