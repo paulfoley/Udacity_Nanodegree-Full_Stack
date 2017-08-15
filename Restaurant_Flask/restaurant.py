5a72378b04ae05713187701b5f5ed3ecd0ea5187
Paul Foley <me@paulnicholasfoley.com>
2017-08-14 19:23:31 -0600

    Update Projects

--- Restaurant_Flask/restaurant.py
+++ Restaurant_Flask/restaurant.py
@@ -1,3 +1,4 @@
+# Create Restaurant Classes
 
 ## Imports
 import os
@@ -9,19 +10,38 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class Restaurant(Base):
-	__tablename__ = 'restaurant'
+	__tablename__ = 'Restaurant'
 	name = Column(String(250), nullable = False)
 	id = Column(Integer, primary_key = True)
 
+	@property
+	def serialize(self):
+		""" Return object data in easily serializeable format """
+		return {
+			'name': self.name,
+			'id': self.id,
+		}
+
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
-	name = Column(String(80), nullable = False)
+	__tablename__ = 'Menu_Item'
 	id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable = False)
 	course = Column(String(250))
 	description = Column(String(250))
 	price = Column(String(8))
-	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+	restaurant_id = Column(Integer, ForeignKey('Restaurant.id'))
 	restaurant = relationship(Restaurant)
+
+	@property
+	def serialize(self):
+		""" Returns object data in easily serializeable format """
+		return {
+			'name': self.name,
+			'description': self.description,
+			'id': self.id,
+			'price': self.price,
+			'course': self.course,
+		}
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
